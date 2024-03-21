f3b805f223878c8a9145849d086b5af893d73f5f
selahaddin599 <selahaddindogan38@gmail.com>
2024-03-21 13:55:33 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp20/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cevrilen = new System.Windows.Forms.RichTextBox();
+            this.cevrilmis = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cevrilen
+            // 
+            this.cevrilen.Location = new System.Drawing.Point(4, 27);
+            this.cevrilen.Name = "cevrilen";
+            this.cevrilen.Size = new System.Drawing.Size(276, 223);
+            this.cevrilen.TabIndex = 0;
+            this.cevrilen.Text = "";
+            this.cevrilen.TextChanged += new System.EventHandler(this.cevrilen_TextChanged);
+            // 
+            // cevrilmis
+            // 
+            this.cevrilmis.EnableAutoDragDrop = true;
+            this.cevrilmis.Location = new System.Drawing.Point(286, 27);
+            this.cevrilmis.Name = "cevrilmis";
+            this.cevrilmis.Size = new System.Drawing.Size(276, 223);
+            this.cevrilmis.TabIndex = 1;
+            this.cevrilmis.Text = "";
+            this.cevrilmis.UseWaitCursor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Türkçe(Turkish)",
+            "İngilizce(English)"});
+            this.comboBox1.Location = new System.Drawing.Point(33, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(99, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dil:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(286, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Dil:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Türkçe(Turkish)",
+            "İngilizce(English)"});
+            this.comboBox2.Location = new System.Drawing.Point(318, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(99, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cevrilmis);
+            this.Controls.Add(this.cevrilen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox cevrilen;
+        private System.Windows.Forms.RichTextBox cevrilmis;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ WindowsFormsApp20/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp20
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> kelimeler = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            kelimeler.Add("Cat", "Kedi");
+            kelimeler.Add("Dog", "Köpek");
+        }
+
+        private void cevrilen_TextChanged(object sender, EventArgs e)
+        {
+            cevrilmis.Clear();
+            foreach (var kelime in kelimeler)
+            {
+                if (cevrilen.Text == kelime.Key)
+                {
+                    cevrilmis.Text = kelime.Value;
+                    break;
+                }
+                else
+                {
+                    cevrilmis.Text = "Kelime Bulunamadı";
+                }
+            }
+        }
+    }
+}
